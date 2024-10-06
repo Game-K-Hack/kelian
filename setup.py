5e36fe94592716496c9255ffd6bdce8905c546b4
Game K <hack.h@laposte.net>
2024-10-06 21:58:53 +0200

    fix: update version for patch release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="kelian",
-    version="0.1.0",
+    version="0.1.1",
     author="Kelian",
     description="Bibliothèque de bouts de code utiles",
     long_description=long_description,
