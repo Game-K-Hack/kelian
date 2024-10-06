826ec9790e1943d7cdb891dd204e12120fb91873
Game K <hack.h@laposte.net>
2024-10-06 22:37:09 +0200

    update gitignore and init

--- src/kelian/__init__.py
+++ src/kelian/__init__.py
@@ -23,7 +23,7 @@ __version__ = "0.1.0"
 __author__ = "Kelian"
 __license__ = "MIT"
 
-from .encryption import alpha2dict, list2dict, encrypt, decrypt, encrypt_by_list, decrypt_by_list
+from .encryption import alpha2dict, list2dict, encrypt, decrypt, encrypt_by_list, decrypt_by_list, encrypt_by_character_manga, decrypt_by_character_manga
 from .system import get_processor_details, get_motherboard_details, get_gpu_details, get_monitor_details, get_cd_drive_details, get_mouse_details, get_speaker_details, get_keyboard_details, get_hard_disk_details, get_ram_details
 from .utils import string2hash
 
@@ -35,6 +35,8 @@ __all__ = [
     "decrypt", 
     "encrypt_by_list", 
     "decrypt_by_list", 
+    "encrypt_by_character_manga", 
+    "decrypt_by_character_manga", 
     "get_processor_details", 
     "get_motherboard_details", 
     "get_gpu_details", 
