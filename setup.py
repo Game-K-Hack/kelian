c0c2ccecb03c0f9ae9b6e6497617f64e2aeecb31
Game K <hack.h@laposte.net>
2024-10-06 22:31:24 +0200

    publish new version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="kelian",
-    version="0.1.1",
+    version="0.1.2",
     author="Kelian",
     description="Bibliothèque de bouts de code utiles",
     long_description=long_description,
